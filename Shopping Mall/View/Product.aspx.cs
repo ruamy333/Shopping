40b187b847f7cca6b8061325cd528308a46c972b
Lu <channinglulingwei@gmail.com>
2015-07-08 17:56:11 +0800

    0708新增回傳值get，並連結購物車

--- Shopping Mall/View/Product.aspx.cs
+++ Shopping Mall/View/Product.aspx.cs
@@ -15,16 +15,26 @@ namespace Shopping_Mall.View
         public DBFunction db = new DBFunction("product");
         protected void Page_Load(object sender, EventArgs e)
         {
-            
-            String ID = Request.QueryString["d"];
-            db.delete("ID", ID);
 
+            //0708每次load都先判斷是否有回傳值，第一次開網頁並沒有回傳
+            String del = Request.QueryString["d"];
+            if (del != null)
+            {
+                db.delete("ID", del);
+            }
             
             String num = Request.QueryString["num"];
-            DBFunction db2 = new DBFunction("purchaseList");
-            String [] input={"ID","account","product_name","price","num"};
-            String[] value = { "19", "root", "PALM OTDR: FIBERPAL OT-8600", "25600", num };
-            db2.insert(input,value);
+            String ID = Request.QueryString["ID"];
+            if (num!=null)
+            {
+                String[][] str = db.searchRowByColumn("ID,name,price", "ID", ID);
+                DBFunction db2 = new DBFunction("purchaseList");
+                String[] value = new String[] { str[0][0], (String)Session["account"], str[0][1], str[0][2], num };
+                //String[] value = { str[0][0], str[0][1], str[0][2], str[0][3], num };
+                String[] attribute = { "ID", "account", "product_name", "price", "num" };
+                db2.insert(attribute, value);
+            }          
+            
             setLeftBar();
             String[][] array;
             if (Request.QueryString["t"] == null)
@@ -35,8 +45,8 @@ namespace Shopping_Mall.View
                 {
                 array = db.searchByRow("type", Request.QueryString["t"]);
                     }
-            
             //0707 新增
+            //0708 修改可以回傳ID、購買數量給自己這頁
             for (int a = 0; a <= (array.Length / 2); a++)
             {
                 if (2 * a == array.Length)
@@ -51,7 +61,7 @@ namespace Shopping_Mall.View
                             + "<div class='delete'><a href='Product.aspx?d=" + array[2 * a + b][0] + "'><img src=../Picture/delete.png style='width:50px;'></a></div>"
                             + "<div class='name'><a href='ProductInformation.aspx?p=" + array[2 * a + b][0] + "'>" + array[2 * a + b][1] + "</a></div>"
                             + "<div class='information'><b style='font-size=0.5cm'>價格：</b>" + array[2 * a + b][3] + "元<b style='font-size=0.5cm;padding-left:35px;'>數量：</b>" + array[2 * a + b][4] + "</div>"
-                            + "<div class='information'><form action='Product.aspx' method='get' >購買數量：<input type='number' name='num' runat'server'><br><input type='submit' value='Submit'></form></div>"
+                            + "<div class='information'><form action='Product.aspx' method='get' >購買數量：<input type='number' name='num' runat'server'><input type='hidden' name='ID' value='"+array[2*a+b][0]+"' runat'server'><br><input type='submit' value='Submit'></form></div>"
                             + "</div>";
                 }
 
