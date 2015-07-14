6ec8fafcf989d1c108d6afe223c5406937471933
ruamy333 <SmallLie1203@gmail.com>
2015-07-14 14:22:52 +0800

    修改連結

--- Shopping Mall/Site.Master.cs
+++ Shopping Mall/Site.Master.cs
@@ -10,22 +10,26 @@ namespace Shopping_Mall
     public partial class Site : System.Web.UI.MasterPage
     {
         public String linkStr;
+        public String Path;
         protected void Page_Load(object sender, EventArgs e)
         {
+            Path = Request.ApplicationPath;
+            if (Path != "/")
+                Path += "/";
             if (Session["account"] == null)
             {
-                linkStr = "<li id='login'><a href='/View/Login.aspx'>Login</a></li>";
+                linkStr = "<li id='login'><a href='" + Path + "View/Login.aspx'>Login</a></li>";
             }
             else if (Session["account"].ToString().Equals("admin"))
             {
-                linkStr = "<li id='producteditor'><a href='/View/ProductEditor.aspx'>Add Product</a></li>"
-                        + "<li id='order'><a href='/View/Orders.aspx'>Order</a></li>"
-                        + "<li id='logout'><a href='/Index.aspx?l=0'>Logout</a></li>";
+                linkStr = "<li id='producteditor'><a href='" + Path + "View/ProductEditor.aspx'>Add Product</a></li>"
+                        + "<li id='order'><a href='" + Path + "View/Orders.aspx'>Order</a></li>"
+                        + "<li id='logout'><a href='" + Path + "Index.aspx?l=0'>Logout</a></li>";
             }
             else
             {
-                linkStr = "<li id='purchasecar'><a href='/View/PurchaseCar.aspx'>Shopping car</a></li>"
-                        + "<li id='logout'><a href='/Index.aspx?l=0'>Logout</a></li>";
+                linkStr = "<li id='purchasecar'><a href='" + Path + "View/PurchaseCar.aspx'>Shopping car</a></li>"
+                        + "<li id='logout'><a href='" + Path + "Index.aspx?l=0'>Logout</a></li>";
             }
             setMenuLink();
         }
--- Shopping Mall/View/ProductEditor.aspx
+++ Shopping Mall/View/ProductEditor.aspx
@@ -1,6 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ProductEditor.aspx.cs" Inherits="Shopping_Mall.ProductManage" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-    <script src="../../js/jquery-2.1.4.min.js"></script>
     <link href="../Css/Common.css" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="Css/ProductEditor.css" />
 </asp:Content>
