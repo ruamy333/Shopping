a7753376853f1e26d7ce8daf525fc53a125d4d7b
ruamy333 <SmallLie1203@gmail.com>
2015-08-03 16:02:55 +0800

    fix leftbar effect

--- Shopping Mall/js/lefrbar-effect.js
+++ Shopping Mall/js/lefrbar-effect.js
@@ -1,26 +1,27 @@
 ﻿
 $(document).ready(function () {
-    $('.leftbar-type').click(function () {
-        var $ul = $(this).next('.leftbar ul');
-        if (!$ul.is(':visible')) {
-            $('.leftbar ul:visible').slideUp();
-        }
-        $ul.slideToggle();
-    }).siblings('ul').hide();
+    $('.leftbar ul').hide();
+    //$('.leftbar-type').click(function () {
+    //    var $ul = $(this).next('.leftbar ul');
+    //    if (!$ul.is(':visible')) {
+    //        $('.leftbar ul:visible').slideUp();
+    //    }
+    //    $ul.slideToggle();
+    //}).siblings('ul').hide();
 
-    $('.column-retractable').click(function () {
-        var $ul = $(this).next('.column-boxL ul');
-        if (!$ul.is(':visible')) {
-            $('.column-boxL ul:visible').slideUp();
-        }
-        $ul.slideToggle();
-    }).siblings('ul').hide();
+    //$('.column-retractable').click(function () {
+    //    var $ul = $(this).next('.column-boxL ul');
+    //    if (!$ul.is(':visible')) {
+    //        $('.column-boxL ul:visible').slideUp();
+    //    }
+    //    $ul.slideToggle();
+    //}).siblings('ul').hide();
 
-    $('.column-retractable').click(function () {
-        var $ul = $(this).next('.column-boxS ul');
-        if (!$ul.is(':visible')) {
-            $('.column-boxS ul:visible').slideUp();
-        }
-        $ul.slideToggle();
-    }).siblings('ul').hide();
+    //$('.column-retractable').click(function () {
+    //    var $ul = $(this).next('.column-boxS ul');
+    //    if (!$ul.is(':visible')) {
+    //        $('.column-boxS ul:visible').slideUp();
+    //    }
+    //    $ul.slideToggle();
+    //}).siblings('ul').hide();
 });
